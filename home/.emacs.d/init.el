d81855294fc088ffb2de25f092726ef0c820211c
Rafał Kołucki <BluRaf@gmx.com>
2020-02-02 13:41:28 +0100

    Emacs font

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,21 +1,3 @@
-;; Default font
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(package-selected-packages (quote (elcord graphene)))
- '(show-paren-mode t)
- '(tool-bar-mode nil)
- '(menu-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "GohuFont" :foundry "Gohu" :slant normal :weight normal :height 105 :width normal)))))
-
 ;; MELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
@@ -27,3 +9,24 @@
 ;; Elcord
 (require 'elcord)
 (elcord-mode)
+
+;; Default font
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(graphene-default-font "Gohufont-14")
+ '(graphene-fixed-pitch-font "Gohufont-14")
+ '(graphene-variable-pitch-font "Unifont-14")
+ '(menu-bar-mode nil)
+ '(package-selected-packages (quote (elcord graphene)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
